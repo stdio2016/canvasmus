34bf0e0b92212be1c9ca9ae8db948f3be12044e6
stdio2016 <randomfrank8@gmail.com>
2016-12-22 18:02:22 +0800

    add Note and Pitch model

---
+++ js/model/Note.js
@@ -0,0 +1,4 @@
+function Note(pitch, type){
+  this.pitch = pitch || []; // Array of pitch
+  this.type = type || 4; // quarter note
+}
---
+++ js/model/Pitch.js
@@ -0,0 +1,10 @@
+﻿function Pitch(step, octave, alter){
+  this.step = step || 1; // from Do(1) to Si(7)
+  if(octave === 0){
+    this.octave = octave;
+  }
+  else {
+    this.octave = octave || 4; // C4 is middle C
+  }
+  this.alter = alter || 0;
+}
